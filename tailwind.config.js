95cf1bbd99bb71956809373d67fe464b5fc6f2d2
Samuel Yuma K <samuelyuma.117@gmail.com>
2023-08-10 15:01:26 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./public/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                'primary-red': '#FC4D40',
+                'secondary-blue': '#058ECE',
+                'secondary-blue-hover': '#36A4D8',
+                'accent-yellow': '#FCCB1D',
+                'accent-yellow-hover': '#FDD54A',
+                'main-black': '#313638',
+                'main-white': '#E8E9EB',
+            },
+            fontFamily: {
+                khand: ['"Khand"', 'sans-serif'],
+                jetbrains: ['"JetBrains Mono"', 'monospace'],
+            },
+            spacing: {
+                26: '110px',
+                'about-photo': '89%',
+            },
+        },
+    },
+    plugins: [],
+}
