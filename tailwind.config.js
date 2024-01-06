7d74a787957182fd5f3255c092139341188d5b1a
Samuel Yuma K <samuelyuma.117@gmail.com>
2024-01-06 12:43:25 +0700

    fix: game won't load

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./public/**/*.{html,js}'],
+    content: ["./src/**/*.{html,js}", "./index.html"],
     theme: {
         extend: {
             colors: {
-                'primary-red': '#FC4D40',
-                'secondary-blue': '#058ECE',
-                'secondary-blue-hover': '#36A4D8',
-                'accent-yellow': '#FCCB1D',
-                'accent-yellow-hover': '#FDD54A',
-                'main-black': '#313638',
-                'main-white': '#E8E9EB',
+                "primary-red": "#FC4D40",
+                "secondary-blue": "#058ECE",
+                "secondary-blue-hover": "#36A4D8",
+                "accent-yellow": "#FCCB1D",
+                "accent-yellow-hover": "#FDD54A",
+                "main-black": "#313638",
+                "main-white": "#E8E9EB",
             },
             fontFamily: {
-                khand: ['"Khand"', 'sans-serif'],
-                jetbrains: ['"JetBrains Mono"', 'monospace'],
+                khand: ['"Khand"', "sans-serif"],
+                jetbrains: ['"JetBrains Mono"', "monospace"],
             },
             spacing: {
-                34: '132px',
-                'about-photo': '83%',
+                34: "132px",
+                "about-photo": "83%",
             },
         },
     },
     plugins: [],
-}
+};
